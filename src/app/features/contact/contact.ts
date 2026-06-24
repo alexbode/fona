e2febe73f8d343b77b4cf6cd8714c1b79ce0a11c
Alex <alexvincentbode@gmail.com>
2026-06-24 11:55:48 -0700

    remove 'bask to home' button from privacy, contact, and terms.

--- src/app/features/contact/contact.ts
+++ src/app/features/contact/contact.ts
@@ -1,24 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { AppRoutesHelper } from '@app/app.routes';
 import { ButtonDirective } from '@app/directive/button';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { HlmIcon } from '@spartan-ng/helm/icon';
-import { lucideChevronLeft, lucideExternalLink, lucideGithub } from '@ng-icons/lucide';
+import { lucideExternalLink, lucideGithub } from '@ng-icons/lucide';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [RouterLink, ButtonDirective, NgIcon, HlmIcon],
+  imports: [ButtonDirective, NgIcon, HlmIcon],
   providers: [
     provideIcons({
-      lucideChevronLeft,
       lucideExternalLink,
       lucideGithub,
     }),
   ],
   templateUrl: './contact.html',
 })
-export class Contact {
-  readonly appRoutesHelper = AppRoutesHelper;
-}
+export class Contact {}
--- src/app/features/privacy/privacy.ts
+++ src/app/features/privacy/privacy.ts
@@ -1,21 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { AppRoutesHelper } from '@app/app.routes';
-import { NgIcon, provideIcons } from '@ng-icons/core';
-import { HlmIcon } from '@spartan-ng/helm/icon';
-import { lucideChevronLeft } from '@ng-icons/lucide';
 
 @Component({
   selector: 'app-privacy',
   standalone: true,
-  imports: [RouterLink, NgIcon, HlmIcon],
-  providers: [
-    provideIcons({
-      lucideChevronLeft,
-    }),
-  ],
+  imports: [],
   templateUrl: './privacy.html',
 })
-export class Privacy {
-  readonly appRoutesHelper = AppRoutesHelper;
-}
+export class Privacy {}
